750417f5f431165884cf4e791f2c0046871f5322
sujphu <60501161+sujanphuyal@users.noreply.github.com>
2022-04-07 22:48:19 +0545

    Simple Counter App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import {useState} from 'react'
 function App() {
+
+  const [value, setValue]= useState(0);
+
+  const onIncrement = () =>{
+    setValue(value+1)
+  }
+
+  const onDecrement = () =>{
+    setValue(value-1)
+  }
+
+  const onReset = () =>{
+    setValue(0)
+  }
+
   return (
-    <div>Counter App</div>
-  );
+    <div className='main'>
+      <div className='center'>
+      <h1>Counter App</h1>
+      <h2>{value}</h2>
+      <div className='btn'>
+      <button onClick={onIncrement}>Increase</button>
+      <button onClick={onDecrement} disabled={value===0}>Decrease</button>
+      <button onClick={onReset} disabled={value===0}>Reset</button>
+    </div>
+    </div>
+    </div>
+
+  )
 }
 
 export default App;
